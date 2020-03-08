f49a958700a8ce7ade8a1f616d878dd46f6b3209
doraemon00 <26552699@qq.com>
2020-03-09 00:20:44 +0800

    add header 组件

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <v-header></v-header>
+    <v-header :seller="seller"></v-header>
     <div class="tab border-1px">
       <div class="tab-item">
         <router-link to="/goods">商品</router-link>
@@ -17,49 +17,46 @@
 </template>
 
 <script>
-import header from './components/header/header'
+import header from "./components/header/header";
 
-const ERR_OK = 0
+const ERR_OK = 0;
 
 export default {
-  data(){
-    return{
-      seller:{}
-    }
+  data() {
+    return {
+      seller: {}
+    };
   },
-  created(){
-    this.$http.get('/api/seller').then((res)=>{
-      if(res.errno === ERR_OK){
-        this.seller = res.data
-        console.log(this.seller)
+  created() {
+    this.$http.get("/api/seller").then(res => {
+      if (res.data.errno === ERR_OK) {
+        console.log(res);
+        this.seller = res.data.data;
       }
-      
-    })
+    });
   },
-  components:{
-    'v-header':header
+  components: {
+    "v-header": header
   }
-}
+};
 </script>
 
-<style lang="stylus">
-  @import "./common/stylus/mixin.styl"
-
-    .tab
-      display: flex
-      width: 100%
-      height: 40px
-      line-height: 40px
-      // border-bottom: 1px solid rgba(7,17,27,0.1)
-      border-1px(rgba(7,17,27,0.1))
-      .tab-item
-        flex: 1
-        text-align: center
-        & > a
-          display:block
-          font-size: 14px
-          color: rgb(77,85,93)
-          &.active
-            color: rgb(240,20,20)
-
+<style lang="stylus" >
+@import './common/stylus/mixin.styl'
+.tab
+  display: flex
+  width: 100%
+  height: 40px
+  line-height: 40px
+  // border-bottom: 1px solid rgba(7,17,27,0.1)
+  border-1px(rgba(7, 17, 27, 0.1))
+  .tab-item
+    flex: 1
+    text-align: center
+    & > a
+      display: block
+      font-size: 14px
+      color: rgb(77, 85, 93)
+      &.active
+        color: rgb(240, 20, 20)
 </style>
