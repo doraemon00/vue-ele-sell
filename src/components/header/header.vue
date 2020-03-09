ed10277e17ebd9cdf6a38a559e59a162c53fdf01
doraemon00 <26552699@qq.com>
2020-03-09 17:13:03 +0800

    add 弹出层

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -15,17 +15,28 @@
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
-      <div v-if="seller.supports" class="support-count">
+      <div v-if="seller.supports" class="support-count" @click="showDetail">
         <span class="count">{{seller.supports.length}}个</span>
         <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
-    <div class="bulletin-wrapper">
-      <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+    <div class="bulletin-wrapper" @click="showDetail">
+      <span class="bulletin-title"></span>
+      <span class="bulletin-text">{{seller.bulletin}}</span>
       <i class="icon-keyboard_arrow_right"></i>
     </div>
     <div class="background">
-      <img :src="seller.avatar" width="100%" height="100%">
+      <img :src="seller.avatar" width="100%" height="100%" />
+    </div>
+    <div v-show="detailShow" class="detail" @click="closeDetail"> 
+      <div class="detail-wrapper clearfix">
+        <div class="detail-main">
+          <h1 class="name">{{seller.name}}</h1>
+        </div>
+      </div>
+      <div class="detail-close">
+        <i class="icon-close"></i>
+      </div>
     </div>
   </div>
 </template>
@@ -36,6 +47,19 @@ export default {
       type: Object
     }
   },
+  data(){
+    return {
+      detailShow:false
+    }
+  },
+  methods:{
+    showDetail(){
+      this.detailShow = true
+    },
+    closeDetail(){
+      this.detailShow = false
+    }
+  },
   created() {
     this.classMap = ["decrease", "discount", "special", "invoice", "guarantee"];
   }
@@ -44,9 +68,10 @@ export default {
 <style lang="stylus"  rel="stylesheet/stylus">
 @import '../../common/stylus/mixin.styl'
 .header
-  position relative
+  position: relative
+  overflow: hidden
   color: #fff
-  background rgba(7,17,27,.5)
+  background: rgba(7, 17, 27, 0.5)
   .content-wrapper
     position: relative
     padding: 24px 12px 18px 24px
@@ -120,19 +145,19 @@ export default {
         line-height: 24px
         font-size: 10px
   .bulletin-wrapper
-    position relative
+    position: relative
     height: 28px
     line-height: 28px
     padding: 0 22px 0 12px
     white-space: nowrap
     overflow: hidden
     text-overflow: ellipsis
-    background rgba(7,17,27,.2)
+    background: rgba(7, 17, 27, 0.2)
     // font-size: 0
     .bulletin-title
       display: inline-block
       vertical-align: top
-      margin-top: 7px
+      margin-top: 8px
       width: 22px
       height: 12px
       bg-image('bulletin')
@@ -143,19 +168,45 @@ export default {
       margin: 0 4px
       font-size: 10px
     .icon-keyboard_arrow_right
-      position absolute
-      font-size 10px
-      right 12px
-      top 8px
-
+      position: absolute
+      font-size: 10px
+      right: 12px
+      top: 8px
   .background
-    position absolute 
+    position: absolute
+    top: 0
+    left: 0
+    width: 100%
+    height: 100%
+    z-index: -1
+    filter: blur(10px)
+  .detail
+    position fixed
+    z-index 100
     top 0
     left 0
     width 100%
     height 100%
-    z-index -1
-    filter blur(10px)
-    
+    overflow auto
+    background rgba(7,17,27,0.8)
+    .detail-wrapper
+      width 100%
+      min-height 100%
+      .detail-main
+        margin-top 61px
+        padding-bottom 64px
+        .name
+          line-height 16px
+          text-align center
+          font-size 16px
+          font-weight 700
+    .detail-close
+      position relative
+      width 32px
+      height 32px
+      margin -64px auto 0 auto
+      clear both
+      font-size 32px
+
 
 </style>
