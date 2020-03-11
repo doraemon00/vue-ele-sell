d0d452dbf5e96ca6e391dd8ece5b171524777f9e
doraemon00 <26552699@qq.com>
2020-03-11 22:32:03 +0800

    add better-scroll menu左右联动效果完成

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper">
+    <div class="menu-wrapper" ref="menuWrapper">
       <ul>
-        <li v-for="item in goods" :key class="menu-item">
+        <li v-for="(item,index) in goods" :key="index" class="menu-item" :class="{'current':currentIndex === index}"
+        @click="selectMenu(index)">
           <span class="text border-1px">
             <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>
             {{item.name}}
@@ -10,25 +11,25 @@
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper">
+    <div class="foods-wrapper" ref="foodsWrapper">
       <ul>
-        <li v-for="(item,index) in goods" :key="index" class="food-list">
+        <li v-for="(item,index) in goods" :key="index" class="food-list" ref="foodList">
           <h1 class="title">{{item.name}}</h1>
           <ul>
             <li v-for="(food,index) in item.foods" :key="index" class="food-item border-1px">
               <div class="icon">
-                <img width="57" height="54" :src="food.icon" alt="">
+                <img width="57" height="54" :src="food.icon" alt />
               </div>
               <div class="content">
                 <h2 class="name">{{food.name}}</h2>
                 <p class="desc">{{food.description}}</p>
                 <div class="extra">
-                  <span>月售{{food.sellCount}}份</span>
+                  <span class="count">月售{{food.sellCount}}份</span>
                   <span>好评率{{food.rating}}%</span>
                 </div>
                 <div class="price">
-                  <span>￥{{food.price}}</span>
-                  <span v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                  <span class="now">￥{{food.price}}</span>
+                  <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
                 </div>
               </div>
             </li>
@@ -39,6 +40,8 @@
   </div>
 </template>
 <script>
+import BScroll from "better-scroll";
+
 const ERR_OK = 0;
 
 export default {
@@ -49,8 +52,22 @@ export default {
   },
   data() {
     return {
-      goods: []
+      goods: [],
+      listHeight: [],
+      scrollY:0
     };
+  },
+  computed:{
+    currentIndex(){
+      for(let i=0;i<this.listHeight.length;i++){
+        let height1 = this.listHeight[i] 
+        let height2 = this.listHeight[i+1] 
+        if(!height2 || (this.scrollY >= height1 && this.scrollY < height2)){
+          return i;
+        }
+      }
+      return 0
+    }
   },
   created() {
     this.classMap = ["decrease", "discount", "special", "invoice", "guarantee"];
@@ -59,8 +76,43 @@ export default {
       if (res.data.errno === ERR_OK) {
         console.log(res);
         this.goods = res.data.data;
+        this.$nextTick(() => {
+          this._initScroll();
+          this._calculateHeight();
+        });
       }
     });
+  },
+  methods: {
+    selectMenu(index){
+      console.log(index)
+      let foodList = this.$refs.foodList
+      let el = foodList[index]
+      this.foodsScroll.scrollToElement(el,300)
+    },
+    _initScroll() {
+      this.menuScroll = new BScroll(this.$refs.menuWrapper, {
+        click:true
+      });
+
+      this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
+        probeType:3
+      });
+
+      this.foodsScroll.on('scroll',(pos)=>{
+        this.scrollY = Math.abs(Math.round(pos.y))
+      })
+    },
+    _calculateHeight() {
+      let foodList = this.$refs.foodList
+      let height = 0;
+      this.listHeight.push(height);
+      for (let i = 0; i < foodList.length; i++) {
+        let item = foodList[i]
+        height+=item.clientHeight;
+        this.listHeight.push(height)
+      }
+    }
   }
 };
 </script>
@@ -83,6 +135,14 @@ export default {
       width: 56px
       padding: 0 12px
       line-height: 14px
+      &.current
+        position relative
+        z-index 10
+        margin-top -1px
+        background #fff
+        font-weight 700
+        .text
+          border-none()
       .icon
         display: inline-block
         vertical-align: top
@@ -110,32 +170,51 @@ export default {
   .foods-wrapper
     flex: 1
     .title
-      padding-left 14px
-      height 26px
-      line-height 26px
-      border-left 2px solid #d9dde1
-      font-size 12px
-      color rgb(147,153,159)
-      background #f3f5f7
+      padding-left: 14px
+      height: 26px
+      line-height: 26px
+      border-left: 2px solid #d9dde1
+      font-size: 12px
+      color: rgb(147, 153, 159)
+      background: #f3f5f7
     .food-item
-      display flex
-      margin 18px
-      padding-bottom 18px
+      display: flex
+      margin: 18px
+      padding-bottom: 18px
       border-1px(rgba(7, 17, 27, 0.1))
-      &.last-child
+      &:last-child
         border-none()
-        margin-bottom 0
+        margin-bottom: 0
       .icon
-        flex 0 0 57px
-        margin-right 10px
+        flex: 0 0 57px
+        margin-right: 10px
       .content
-        flex 1
+        flex: 1
         .name
-          margin:2px 0 8px 0
-          height 14px
-          line-height 14px
-          font-size 14px
-          color rgb(7,17,27)
-          
-
+          margin: 2px 0 8px 0
+          height: 14px
+          line-height: 14px
+          font-size: 14px
+          color: rgb(7, 17, 27)
+        .desc, .extra
+          line-height: 10px
+          font-size: 10px
+          color: rgb(147, 153, 159)
+        .desc
+          margin-bottom: 8px
+          line-height: 12px
+        .extra
+          .count
+            margin-right: 12px
+        .price
+          font-weight: 700
+          line-height: 24px
+          .now
+            margin-right: 8px
+            font-size: 14px
+            color: rgb(240, 20, 20)
+          .old
+            text-decoration: line-through
+            font-size: 10px
+            color: rgb(147, 153, 159)
 </style>
