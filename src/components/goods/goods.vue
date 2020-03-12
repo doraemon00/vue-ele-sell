3815ffea131297f142ace92ec0bf919382fbe3d7
doraemon00 <26552699@qq.com>
2020-03-12 17:09:52 +0800

    add cartcontrol 组件

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -31,6 +31,9 @@
                   <span class="now">￥{{food.price}}</span>
                   <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
                 </div>
+                <div class="cartcontrol-wrapper">
+                  <cartcontrol :food="food"></cartcontrol>
+                </div>
               </div>
             </li>
           </ul>
@@ -43,6 +46,7 @@
 <script>
 import BScroll from "better-scroll";
 import shopcart from 'components/shopcart/shopcart';
+import cartcontrol from 'components/cartcontrol/cartcontrol'
 
 const ERR_OK = 0;
 
@@ -98,6 +102,7 @@ export default {
       });
 
       this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
+        click:true,
         probeType:3
       });
 
@@ -118,7 +123,8 @@ export default {
     }
   },
   components:{
-    shopcart
+    shopcart,
+    cartcontrol
   }
 };
 </script>
@@ -223,4 +229,8 @@ export default {
             text-decoration: line-through
             font-size: 10px
             color: rgb(147, 153, 159)
+        .cartcontrol-wrapper
+          position: absolute
+          right: 0
+          bottom: 12px
 </style>
