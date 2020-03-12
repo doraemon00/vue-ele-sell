e2180c7e985fefa0744df3bdba11e52bf0ffa646
doraemon00 <26552699@qq.com>
2020-03-12 17:29:38 +0800

    update cartcontrol 动画 联动

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="cartcontrol">
-    <div class="cart-decrease icon-remove_circle_outline" v-show="food.count > 0" @click="decreaseCart">
-      <!-- <span class="icon-remove_circle_outline"></span> -->
-    </div>
+    <transition name="move">
+      <div
+        class="cart-decrease"
+        v-show="food.count > 0"
+        @click="decreaseCart"
+      >
+        <span class="inner icon-remove_circle_outline"></span>
+      </div>
+    </transition>
     <div class="cart-count" v-show="food.count > 0">{{food.count}}</div>
     <div class="cart-add icon-add_circle" @click="addCart"></div>
   </div>
 </template>
 <script>
-import Vue from 'vue'
+import Vue from "vue";
 export default {
   props: {
     food: {
@@ -18,19 +24,19 @@ export default {
   created() {
     // console.log(this.food);
   },
-  methods:{
-    addCart(){
-      console.log("click")
-      if(!this.food.count){
-        Vue.set(this.food,'count',1)
+  methods: {
+    addCart() {
+      console.log("click");
+      if (!this.food.count) {
+        Vue.set(this.food, "count", 1);
         // this.food.count = 1
-      }else{
-        this.food.count++
+      } else {
+        this.food.count++;
       }
     },
-    decreaseCart(){
-      if(this.food.count){
-        this.food.count-- 
+    decreaseCart() {
+      if (this.food.count) {
+        this.food.count--;
       }
     }
   }
@@ -39,22 +45,38 @@ export default {
 <style lang="stylus">
 .cartcontrol
   font-size: 0
-  .cart-decrease, .cart-add
+  .cart-decrease
+    display: inline-block
+    padding 6px
+    opacity: 1
+    transform: translate3d(0, 0, 0)
+    .inner
+      display inline-block
+      line-height 24px
+      font-size 24px
+      color rgb(0,160,220)
+      transition all 0.4s linear
+      transform rotate(0)
+    &.move-enter-active, &.move-leave-active
+      transition: all 0.4s linear
+    &.move-enter,&.move-leave-active
+      opacity 0
+      transform translate3d(24px,0,0)
+      .inner
+        transform rotate(180deg)
+  .cart-count
+    display: inline-block
+    vertical-align: top
+    width: 12px
+    padding-top: 6px
+    line-height: 24px
+    text-align: center
+    font-size: 10px
+    color: rgb(147, 153, 159)
+  .cart-add
     display: inline-block
     padding: 6px
     line-height: 24px
     font-size: 24px
-    color rgb(0,160,220)
-  .cart-count
-    display: inline-block
-    vertical-align top
-    width 12px
-    padding-top 6px
-    line-height 24px
-    text-align center
-    font-size 10px
-    color rgb(147,153,159)
-  .cart-add
-    display: inline-block
-    
+    color: rgb(0, 160, 220)
 </style>
--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -40,7 +40,7 @@
         </li>
       </ul>
     </div>
-    <shopcart :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>
+    <shopcart :select-foods="selectFoods" :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>
   </div>
 </template>
 <script>
@@ -73,6 +73,17 @@ export default {
         }
       }
       return 0
+    },
+    selectFoods(){
+      let foods = []
+      this.goods.forEach((good)=>{
+        good.foods.forEach((food)=>{
+          if(food.count){
+            foods.push(food)
+          }
+        })
+      })
+      return foods
     }
   },
   created() {
